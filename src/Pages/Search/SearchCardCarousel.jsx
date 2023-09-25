0836265d579d726d9a42f8efaa4ca70b25b2d4cc
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-09-25 21:00:24 +0100

    ID007 [FE] - Added Favourite Icon

--- src/Pages/Search/SearchCardCarousel.jsx
+++ src/Pages/Search/SearchCardCarousel.jsx
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import './SearchCardCarousel.css'
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 
 export default function SearchCardCarousel() {
+    const [isLiked, setIsLiked] = useState(false);
+
+    const handleLike = () => {
+        setIsLiked(!isLiked);
+    }
+
     const settings = {
         dots: true,
         infinite: true,
@@ -15,7 +23,7 @@ export default function SearchCardCarousel() {
     }
 
     return (
-        <div>
+        <div className="relative">
             <Slider {...settings}>
                 <div>
                     <img alt="blog photo" src="https://www.tailwind-kit.com/images/blog/1.jpg" className="object-cover w-full max-h-40" />
@@ -36,6 +44,16 @@ export default function SearchCardCarousel() {
                     <img alt="blog photo" src="https://www.tailwind-kit.com/images/blog/6.jpg" className="object-cover w-full max-h-40" />
                 </div>
             </Slider>
+            <div>
+                <AiOutlineHeart
+                    className={`text-gray-100 hover:text-white text-3xl z-20 absolute right-4 top-4 transition duration-200 ease-in-out ${isLiked && 'opacity-0 z-0'}`}
+                    onClick={handleLike}
+                />
+                <AiFillHeart
+                    className={`text-rose-600 hover:text-rose-500 text-3xl z-20 absolute right-4 top-4 transition duration-200 ease-in-out ${!isLiked && 'opacity-0 z-0'}`}
+                    onClick={handleLike}
+                />
+            </div>
         </div>
     );
 }
