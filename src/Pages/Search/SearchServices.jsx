ef427b123f6dc4f752e946547e376893be6eb9de
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-17 17:03:02 +0100

    Fixed Search Modals

--- src/Pages/Search/SearchServices.jsx
+++ src/Pages/Search/SearchServices.jsx
@@ -1,85 +1,94 @@
-import { useState, useRef, useEffect } from "react";
+import { useRef, useState } from "react";
+import ClickAwayListener from "react-click-away-listener";
 import RegionModal from "./RegionModal.jsx";
 import CategoryModal from "./CategoryModal.jsx";
 
 export default function SearchServices(props) {
     const [category, setCategory] = useState("");
     const [region, setRegion] = useState("");
-    const [isRegionModalOpen, setIsRegionModalOpen] = useState(false);
-    const regionModalRef = useRef(null);
-
-    function handleRegionClick(region) {
-        setRegion(region);
-    }
+    const [categoryModalOpen, setCategoryModalOpen] = useState(false);
+    const [regionModalOpen, setRegionModalOpen] = useState(false);
+    const ref = useRef(null);
 
     function handleCategoryClick(category) {
         setCategory(category);
     }
 
-    function handleRegionInputChange(e) {
-        setRegion(e.target.value);
+    function handleRegionClick(region) {
+        setRegion(region);
     }
 
     function handleCategoryInputChange(e) {
         setCategory(e.target.value);
     }
 
-    const handleRegionModalOpen = () => {
-        setIsRegionModalOpen(!isRegionModalOpen);
+    function handleRegionInputChange(e) {
+        setRegion(e.target.value);
     }
-
-    useEffect(() => {
-        function handleRegionClickOutside(event) {
-            if (regionModalRef.current && !regionModalRef.current.contains(event.target)) {
-                console.log(regionModalRef.current);
-                console.log(regionModalRef.current.contains(event.target));
-                console.log("cenas2");
-                setIsRegionModalOpen(false);
-            }
-            console.log("cenas");
-        }
-        document.addEventListener('click', handleRegionClickOutside);
-
-        return () => {
-            document.removeEventListener('click', handleRegionClickOutside);
-        };
-    }, []);
 
     return (
         <form className="flex justify-start w-4/4 z-[25]">
 
             <div className="relative w-56 h-14">
+
                 <input
                     value={category}
                     onChange={handleCategoryInputChange}
+                    onClick={(e) => { e.stopPropagation(); setCategoryModalOpen(!categoryModalOpen) }}
                     type="text"
                     id="category"
                     className=" rounded-s-lg flex-1 border border-gray-300 w-56 h-full py-4 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent"
                     placeholder="O que procura?"
                 />
-                <CategoryModal
-                    className="absolute top-[100%+5px] left-0"
-                    Categories={props.Categories}
-                    CategoryClick={handleCategoryClick}
-                />
+                <ClickAwayListener
+                    className="absolute top-[100%+5px] left-0 w-[620px]"
+                    onClickAway={(e) => {
+                        console.log('cenas')
+                        e.stopPropagation();
+                        if (!e.target.contains(ref.current)) {
+
+                            if (categoryModalOpen) {
+                                setCategoryModalOpen(false);
+                            }
+                        }
+                    }}>
+                    <>
+                        <CategoryModal
+                            ref={ref}
+                            className="absolute top-[100%+5px] left-0"
+                            Categories={props.Categories}
+                            CategoryClick={handleCategoryClick}
+                            open={categoryModalOpen}
+                        />
+                    </>
+                </ClickAwayListener>
             </div>
             <div className="relative w-56 h-14">
                 <input
                     value={region}
                     onChange={handleRegionInputChange}
-                    onClick={handleRegionModalOpen}
+                    onClick={(e) => { e.stopPropagation(); setRegionModalOpen(!regionModalOpen) }}
                     type="text"
                     id="location"
                     className="flex-1 border border-gray-300 w-56 h-full py-4 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent"
                     placeholder="Onde?" />
-                {isRegionModalOpen &&
-                    <RegionModal
-                        ref={regionModalRef}
-                        className="absolute top-[100%+5px] left-0"
-                        Regions={props.Regions}
-                        RegionClick={handleRegionClick}
-                        handleRegionModalOpen={handleRegionModalOpen} />
-                }
+                <ClickAwayListener
+                    className="absolute top-[100%+5px] left-0 w-[620px]"
+                    onClickAway={(e) => {
+                        console.log('cenas2')
+                        e.stopPropagation()
+                        setRegionModalOpen(false);
+                    }}>
+                    <>
+                        <RegionModal
+                            className="absolute top-[100%+5px] left-0"
+                            Regions={props.Regions}
+                            RegionClick={handleRegionClick}
+                            open={regionModalOpen}
+                        />
+                    </>
+                </ClickAwayListener>
+
             </div>
             <div className="relative h-14">
                 <button className="flex-shrink-0 h-full px-4 py-2 text-base font-semibold text-white bg-orange-400 rounded-e-lg shadow-md hover:bg-orange-500 focus:ring-orange-500 focus:ring-2 focus:ring-offset-2 focus:ring-offset-orange-200 focus:outline-none" type="submit">
