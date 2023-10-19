75fd636aa7a1cfb6acef0eded86a7bbfb1d91042
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-19 17:01:00 +0100

    ID026 [FE] - Pagina fornecedor

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ import PageUserSettings from "./Pages/UserSettings/PageUserSettings";
 import { RegisterSupplier } from "./Pages/Register/RegisterSupplier";
 import SupplierServices from "./Pages/Supplier/SupplierServices";
 import SupplierServicesForm from "./Pages/Supplier/SupplierServicesForm";
+import SupplierPage from "./Pages/Search/SupplierPage";
 import "./App.css";
 
 export default function App() {
@@ -24,21 +25,22 @@ export default function App() {
   return (
     <>
       {layoutRender && <Header />}
-        <Routes>
-            <Route path="/" element={<PageHome />} />
-            <Route path="/settings" element={<PageUserSettings />} />
-            <Route path="/supplier/services" element={<SupplierServices  />} />
-            <Route path="/supplier/services/edit/:id" element={<SupplierServicesForm  />} />
-            <Route path="/supplier/services/new" element={<SupplierServicesForm  />} />
-            <Route path="/search" element={<PageSearch />} />
+      <Routes>
+        <Route path="/" element={<PageHome />} />
+        <Route path="/settings" element={<PageUserSettings />} />
+        <Route path="/supplier/services" element={<SupplierServices />} />
+        <Route path="/supplier/services/edit/:id" element={<SupplierServicesForm />} />
+        <Route path="/supplier/services/new" element={<SupplierServicesForm />} />
+        <Route path="/search" element={<PageSearch />} />
+        <Route path="supplier" element={<SupplierPage />} />
 
-            <Route path="/signin" element={<Register />} />
-            <Route path="/signin/supplier" element={<RegisterSupplier />} />
-            <Route path="/login" element={<Login />} />
+        <Route path="/signin" element={<Register />} />
+        <Route path="/signin/supplier" element={<RegisterSupplier />} />
+        <Route path="/login" element={<Login />} />
 
-            {/* Other Routes */}
-            <Route path="*" element={<Navigate to="/" replace />}/>
-        </Routes>
+        {/* Other Routes */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
       {layoutRender && <Footer />}
     </>
   );
