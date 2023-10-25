2f728d5e68eea00fe6506a00cb338d52e197e644
Rui Teixeira <rui.teixeira.t0123945@edu.atec.pt>
2023-10-25 17:27:51 +0100

    Supplier Socials fixes

--- src/Pages/Supplier/Settings/SupplierContacts.jsx
+++ src/Pages/Supplier/Settings/SupplierContacts.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getUser, updateUser } from '../../../API/User';
 
-import { FaFacebook, FaInstagram, FaLink, FaLinkedin, FaPinterest } from 'react-icons/fa';
+import { FaFacebook, FaInstagram, FaLink, FaLinkedin, FaPinterest, FaSpinner } from 'react-icons/fa';
 
 
 export default function SupplierContacts(props) {
@@ -9,6 +9,16 @@ export default function SupplierContacts(props) {
         const websitePattern = /^(https?|http):\/\/[^\s/$.?#].[^\s]*$/i;;
         const [user, setUser] = useState({});
         const [isLoading, setIsLoading] = useState(true);
+        const [isSubmitingSocials, setIsSubmitingSocials] = useState(false);
+        const [social, setSocial] = useState({
+                facebook: '',
+                instagram: '',
+                pinterest: '',
+                linkedin: '',
+                website: '',
+                hasEdited: false
+        });
+
         const [errors, setErrors] = useState({
                 facebook: '',
                 instagram: '',
@@ -34,6 +44,13 @@ export default function SupplierContacts(props) {
         useEffect(() => {
                 getUser()
                         .then((response) => {
+                                setSocial({
+                                        facebook: '',
+                                        instagram: '',
+                                        pinterest: '',
+                                        linkedin: '',
+                                        website: ''
+                                })
                                 setUser(response);
                                 console.log(response)
                         })
@@ -45,12 +62,18 @@ export default function SupplierContacts(props) {
                         })
         }, []);
 
+        const handleChange = (prop) => (e) => {
+                setSocial({ ...social, [prop]: e.target.value, hasEdited: true });
+                console.log(social)
+        };
+
         function submitSocials(e) {
                 e.preventDefault();
+                setIsSubmitingSocials(true);
                 let hasErrors = false;
                 clearErrors();
 
-                const newSocials = {
+                const socials = {
                         facebook: e.target.facebook.value,
                         instagram: e.target.instagram.value,
                         pinterest: e.target.pinterest.value,
@@ -58,50 +81,60 @@ export default function SupplierContacts(props) {
                         website: e.target.website.value
                 }
 
-                if (newSocials.facebook.includes('facebook.com/')) {
-                        newSocials.facebook = newSocials.facebook.split('facebook.com/')[1];
+                if (socials.facebook.includes('facebook.com/')) {
+                        socials.facebook = socials.facebook.split('facebook.com/')[1];
                 }
-                if (!usernamePattern.test(newSocials.facebook) && newSocials.facebook !== '') {
+                if (!usernamePattern.test(socials.facebook) && socials.facebook !== '') {
                         updateError('facebook', 'Nome de utilizador inválido');
                         hasErrors = true;
                 }
 
-                if (newSocials.instagram.includes('instagram.com/')) {
-                        newSocials.instagram = newSocials.instagram.split('instagram.com/')[1];
+                if (socials.instagram.includes('instagram.com/')) {
+                        socials.instagram = socials.instagram.split('instagram.com/')[1];
                 }
-                if (!usernamePattern.test(newSocials.instagram) && newSocials.instagram !== '') {
+                if (!usernamePattern.test(socials.instagram) && socials.instagram !== '') {
                         updateError('instagram', 'Nome de utilizador inválido');
                         hasErrors = true;
                 }
 
-                if (newSocials.pinterest.includes('pinterest.pt/')) {
-                        newSocials.pinterest = newSocials.pinterest.split('pinterest.pt/')[1];
+                if (socials.pinterest.includes('pinterest.pt/')) {
+                        socials.pinterest = socials.pinterest.split('pinterest.pt/')[1];
                 }
-                if (newSocials.pinterest.includes('pinterest.com/')) {
-                        newSocials.pinterest = newSocials.pinterest.split('pinterest.com/')[1];
+                if (socials.pinterest.includes('pinterest.com/')) {
+                        socials.pinterest = socials.pinterest.split('pinterest.com/')[1];
                 }
-                if (!usernamePattern.test(newSocials.pinterest) && newSocials.pinterest !== '') {
+                if (!usernamePattern.test(socials.pinterest) && socials.pinterest !== '') {
                         updateError('pinterest', 'Nome de utilizador inválido');
                         hasErrors = true;
                 }
 
-                if (newSocials.linkedin.includes('linkedin.com/in/')) {
-                        newSocials.linkedin = newSocials.linkedin.split('linkedin.com/in/')[1];
+                if (socials.linkedin.includes('linkedin.com/in/')) {
+                        socials.linkedin = socials.linkedin.split('linkedin.com/in/')[1];
                 }
-                if (!usernamePattern.test(newSocials.linkedin) && newSocials.linkedin !== '') {
+                if (!usernamePattern.test(socials.linkedin) && socials.linkedin !== '') {
                         updateError('linkedin', 'Nome de utilizador inválido');
                         hasErrors = true;
                 }
 
-                if (!websitePattern.test(newSocials.website) && newSocials.website !== '') {
+                if (!websitePattern.test(socials.website) && socials.website !== '') {
                         updateError('website', 'Website inválido');
                         hasErrors = true;
                 }
 
-                if (hasErrors) return;
+                if (hasErrors) {
+                        setIsSubmitingSocials(false);
+                        return
+                };
 
-                console.log(newSocials)
-                updateUser(newSocials)
+                console.log(socials)
+                updateUser(socials).then(() => {
+                        alert('Redes sociais atualizadas com sucesso!');
+                }).catch((error) => {
+                        alert(error.response.data.error);
+                }).finally(() => {
+                        console.log('cenas')
+                        setIsSubmitingSocials(false);
+                })
         }
 
 
@@ -140,6 +173,7 @@ export default function SupplierContacts(props) {
                                                                                                         className={`rounded-r-lg flex-1 appearance-none border ${errors.facebook === '' ? 'border-gray-300' : 'border-red-600'} w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent transition ease-in duration-200 hover:bg-gray-50`}
                                                                                                         name="facebook"
                                                                                                         defaultValue={user.social.facebook ? user.social.facebook : ''}
+                                                                                                        onChange={handleChange('facebook')}
                                                                                                         placeholder="nome_de_utilizador" />
                                                                                         </div>
                                                                                         {errors.facebook !== '' && <span className="text-red-600 text-sm">{errors.facebook}</span>}
@@ -156,6 +190,7 @@ export default function SupplierContacts(props) {
                                                                                                         className={` rounded-r-lg flex-1 appearance-none border ${errors.instagram === '' ? 'border-gray-300' : 'border-red-600'} w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent transition ease-in duration-200 hover:bg-gray-50`}
                                                                                                         name="instagram"
                                                                                                         defaultValue={user.social.instagram ? user.social.instagram : ''}
+                                                                                                        onChange={handleChange('instagram')}
                                                                                                         placeholder="nome_de_utilizador" />
                                                                                         </div>
                                                                                         {errors.instagram !== '' && <span className=" text-red-600 text-sm">{errors.instagram}</span>}
@@ -172,6 +207,7 @@ export default function SupplierContacts(props) {
                                                                                                         className={` rounded-r-lg flex-1 appearance-none border ${errors.pinterest === '' ? 'border-gray-300' : 'border-red-600'} w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent transition ease-in duration-200 hover:bg-gray-50`}
                                                                                                         name="pinterest"
                                                                                                         defaultValue={user.social.pinterest ? user.social.pinterest : ''}
+                                                                                                        onChange={handleChange('pinterest')}
                                                                                                         placeholder="nome_de_utilizador" />
                                                                                         </div>
                                                                                         {errors.pinterest !== '' && <span className="text-red-600 text-sm">{errors.pinterest}</span>}
@@ -188,6 +224,7 @@ export default function SupplierContacts(props) {
                                                                                                         className={` rounded-r-lg flex-1 appearance-none border ${errors.linkedin === '' ? 'border-gray-300' : 'border-red-600'} w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent transition ease-in duration-200 hover:bg-gray-50`}
                                                                                                         name="linkedin"
                                                                                                         defaultValue={user.social.linkedin ? user.social.linkedin : ''}
+                                                                                                        onChange={handleChange('linkedin')}
                                                                                                         placeholder="nome_de_utilizador" />
                                                                                         </div>
                                                                                         {errors.linkedin !== '' && <span className="text-red-600 text-sm">{errors.linkedin}</span>}
@@ -203,16 +240,25 @@ export default function SupplierContacts(props) {
                                                                                                         className={` rounded-r-lg flex-1 appearance-none border ${errors.website === '' ? 'border-gray-300' : 'border-red-600'} w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-orange-400 focus:border-transparent transition ease-in duration-200 hover:bg-gray-50`}
                                                                                                         name="website"
                                                                                                         defaultValue={user.social.website ? user.social.website : ''}
+                                                                                                        onChange={handleChange('website')}
                                                                                                         placeholder="website.com" />
                                                                                         </div>
                                                                                         {errors.website !== '' && <span className="text-red-600 text-sm">{errors.website}</span>}
 
                                                                                         <div className="flex w-full mt-2 justify-end">
-                                                                                                <button
-                                                                                                        type="submit"
-                                                                                                        className="py-2.5 px-4 text-orange-400 border-orange-400 border hover:bg-orange-400 hover:text-white w-fit transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none rounded-lg ">
-                                                                                                        Guardar Alterações
-                                                                                                </button>
+                                                                                                {social.hasEdited &&
+                                                                                                        <button
+                                                                                                                type="submit"
+                                                                                                                className="py-2.5 px-4 text-orange-400 border-orange-400 border hover:bg-orange-400 hover:text-white w-fit transition ease-in duration-200 text-center text-sm font-semibold shadow-md focus:outline-none rounded-lg "
+                                                                                                                disabled={isSubmitingSocials || !social.hasEdited}
+                                                                                                        >
+                                                                                                                {isSubmitingSocials ? (
+                                                                                                                        <FaSpinner className="animate-spin -ml-1 h-5 w-5 text-white" />
+                                                                                                                ) : (
+                                                                                                                        <span>Guardar</span>
+                                                                                                                )}
+                                                                                                        </button>
+                                                                                                }
                                                                                         </div>
                                                                                 </div>
                                                                         </form>
